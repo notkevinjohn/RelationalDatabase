c5a1c65644839c3af1505f547af3e005e70abc3c
kevin john <notkevinjohn@gmail.com>
2021-11-23 12:22:12 -0800

    controller working to add simple record

--- Controller.py
+++ Controller.py
@@ -1,28 +1,67 @@
 import sqlite3
+from OutputManager import OutputManager
+from termcolor import colored
 
 class Controller():
-	def __init__(self):
-		self.output("Init")
+	def __init__(self, dbfile):
+		self.dbfile = dbfile
+		self.outputManager = OutputManager()
+		self.output("Init "+str(dbfile), "status")
+
 
 	def connect(self):
 		self.output("Connect")
-		self.connection = sqlite3.connect("database.db")
+		try:
+			self.connection = sqlite3.connect("database.db")
+		except Error as e:
+			self.output ("error"+str(e))
 
 	def createTable(self, name, values):
-		cursor = self.connection.cursor()
-		command = "CREATE TABLE "+name+"("
+		command = "CREATE TABLE IF NOT EXISTS "+name+"("
 		valuesSQL = []
 		for value in values:
 			valuesSQL.append(str(value+" "+values[value]))
+		command+=", ".join(valuesSQL)
 		command+=")"
-		self.output(valuesSQL)
+		self.execute(command)
 
+	def listTables(self):
+		cursor = self.connection.cursor()
+		cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+		print (cursor.fetchall())
 
-	def output(self, message):
-		print (message)
+	def addRecord(self, name, values):
+		columnsSQL = []
+		valuesSQL = []
+		for value in values:
+			columnsSQL.append(value)
+			valuesSQL.append("'"+values[value]+"'")
+		command = "INSERT INTO "+name+"("+",".join(columnsSQL)+") VALUES ("+",".join(valuesSQL)+")"
+		self.execute(command)
+
+	def getRecords(self, table):
+		cursor = self.connection.cursor()
+		command = "SELECT * FROM Characters"
+		cursor.execute(command)
+		print (cursor.fetchall())
+
+	def execute(self, command):
+		cursor = self.connection.cursor()
+		self.output(command)
+		cursor.execute(command)
+
+	def commit(self):
+		self.connection.commit()
+
+	def output(self, message, type):
+		self.outputManager.output(message, type)
 
 
 if __name__ == "__main__":
-	controller = Controller()
-	controller.connect()
-	controller.createTable("Characters", {"Name":"TEXT"})
+	controller = Controller("database.db")
+	#controller.connect()
+	#controller.createTable("Characters", {"id":"INTEGER PRIMARY KEY","name":"TEXT","species":"TEXT","description":"TEXT"})
+	#controller.listTables()
+	#controller.addRecord("Characters",{"name":"Ackbar","species":"Mon Calamari","description":"Cool Admiral"})
+	#controller.commit()
+	#controller.getRecords("Characters")
